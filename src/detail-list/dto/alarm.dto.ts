19df4a44f59189896a5a646f8376dcf838324849
Coder <lazymonkey789@gmail.com>
2022-07-14 21:44:38 +0900

    input delete 완료

--- src/detail-list/dto/alarm.dto.ts
+++ src/detail-list/dto/alarm.dto.ts
@@ -4,27 +4,27 @@ export class CreateAlarmDto {
   @IsString()
   readonly LineNo: string;
   @IsString()
-  readonly Worker: string;
+  readonly Worker: string; /* 
   @IsNumber()
   readonly OutCheck: number;
   @IsString()
   readonly Controller: string;
   @IsNumber()
-  readonly DellReport: number;
+  readonly DellReport: number; */
   @IsDateString()
-  readonly ReportTime: Date;
+  readonly ReportTime: Date; /* 
   @IsDateString()
   readonly NmsTime: Date;
   @IsDateString()
   readonly FinishTime: Date;
   @IsString()
-  readonly Spot: string;
+  readonly Spot: string; */
   @IsString()
-  readonly Cause: string;
+  readonly Cause: string; /* 
   @IsString()
   readonly Content: string;
   @IsString()
   readonly Consider: string;
   @IsString()
-  readonly Writer: string;
+  readonly Writer: string; */
 }
